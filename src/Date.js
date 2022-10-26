86ef13c9930866555f24ed53cbf33db81b9afec8
SarahAllison92 <109143210+SarahAllison92@users.noreply.github.com>
2022-10-26 12:15:30 +0100

    Added component for the date and the css

---
+++ src/Date.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./Date.css";
+
+export default function Date() {
+  return (
+    <div className="Date">
+      Last updated: Sunday 21st August <br />
+      12.30
+    </div>
+  );
+}
